efe0b782ae1a0dda2890f34f4656f1e7ce0638d0
Joel Clermont <jclermont@gmail.com>
2014-09-26 16:09:23 -0400

    add an example for rsync based backups of selected folders to S3

--- ansible/files/backup/file-backup.sh
+++ ansible/files/backup/file-backup.sh
@@ -13,3 +13,10 @@ tar cvzf $BACKUPFILE.tgz /vagrant
 s3cmd put $BACKUPFILE.tgz s3://{{ s3_bucket_name }}/$BACKUPFILE.tgz
 # scp $BACKUPFILE.tgz {{ scp_user }}@{{ scp_host }}:/backup
 rm $BACKUPFILE.tgz
+
+# if you don't want to create a full snapshot of your whole site
+# another nice approach is to selectively rsync specific folders
+# up to S3. Especially nice for folders containing client generated
+# content.
+
+#s3cmd sync --recursive /var/www/code/**folder name** s3://{{ s3_bucket_name }}/client-files/
